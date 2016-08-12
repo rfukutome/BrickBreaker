3bd562ead4801012962f82c3b82d678f25160c2f
Richard Fukutome <rfukutome@gmail.com>
2016-08-11 21:18:11 -0700

    Creating level scene, as well as music

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour {
+    //Only want one instance of a BGM player, so we create a singleton
+    static BGM instance = null;
+
+	// Use this for initialization
+
+    void Awake()
+    {
+        //Want this object to persist through scenes.
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+	void Start () {
+
+    }
+}
